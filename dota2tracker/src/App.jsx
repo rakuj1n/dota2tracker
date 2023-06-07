65da9d04d144fc2181eb824541ac8ae876f5bb20
Your Name <you@example.com>
2023-06-07 17:21:28 +0800

    adds routes

--- dota2tracker/src/App.jsx
+++ dota2tracker/src/App.jsx
@@ -1,11 +1,18 @@
 import { useState } from 'react'
-
+import { Routes, Route } from 'react-router-dom'
+import Home from './Pages/Home'
+import Meta from './Pages/Meta'
 
 function App() {
 
 
   return (
     <>
+    <h1>hello</h1>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/meta" element={<Meta />}/>
+      </Routes>
       <h1>hello</h1>
     </>
   )
---
+++ dota2tracker/src/Pages/Meta.jsx
@@ -0,0 +1,7 @@
+export default function Meta() {
+    return (
+        <>
+            <h1>Meta page</h1>
+        </>
+    )
+}

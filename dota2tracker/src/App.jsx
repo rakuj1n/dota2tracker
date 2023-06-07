1d37564793efb22d9e3e619514a15fdbf2a27dce
Your Name <you@example.com>
2023-06-07 17:29:16 +0800

    adds more pages

--- dota2tracker/src/App.jsx
+++ dota2tracker/src/App.jsx
@@ -1,19 +1,20 @@
 import { useState } from 'react'
 import { Routes, Route } from 'react-router-dom'
-import Home from './Pages/Home'
-import Meta from './Pages/Meta'
+import Home from './Pages/Home/Home'
+import Meta from './Pages/Meta/Meta'
+import Teams from './Pages/Teams/Teams'
 
 function App() {
 
 
   return (
     <>
-    <h1>hello</h1>
+    <h1>This is a Navbar with links to home, meta, teams</h1>
       <Routes>
         <Route path="/" element={<Home />}/>
         <Route path="/meta" element={<Meta />}/>
+        <Route path="/teams" element={<Teams />}/>
       </Routes>
-      <h1>hello</h1>
     </>
   )
 }

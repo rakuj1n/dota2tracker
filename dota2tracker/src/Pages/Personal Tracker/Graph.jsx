30bf8ea7dd9352db000470654d9c0d6afb377ae2
Your Name <you@example.com>
2023-06-10 23:46:14 +0800

    adds graph, without fetch data

--- dota2tracker/src/Pages/Personal Tracker/Graph.jsx
+++ dota2tracker/src/Pages/Personal Tracker/Graph.jsx
@@ -1,9 +1,14 @@
-
+import { BarChart, CartesianGrid, XAxis, YAxis, Tooltip, Legend, Bar } from 'recharts'
 
 export default function Graph() {
     return (
-        <>
-        <p>this is a graph</p>
-        </>
+        <div className='graph'>  
+        <BarChart margin={{top: 0, right:0,bottom:0,left:0}} width={300} height={250} data={[{"name":"Wins","pv":15},{"name":"Losses","pv":3}]}>
+            <CartesianGrid horizontal={false} vertical={false} />
+            <XAxis tick={{ fill: '#DDE6ED' }} dataKey="name" />
+            <YAxis tick={{ fill: '#DDE6ED' }}/>
+            <Bar dataKey="pv" fill="#9DB2BF" />
+        </BarChart>
+        </div>
     )
 }
